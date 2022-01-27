474ba1661f27a7c4dfc51fc72d6d9464fb7779fe
clgkd <clgkd@DESKTOP-KHU7TER>
2022-01-27 14:02:55 +0900

    hometest로 연결되도록 변경

--- finalproject0124/src/main/java/com/test/finalproject/controller/HomeController.java
+++ finalproject0124/src/main/java/com/test/finalproject/controller/HomeController.java
@@ -1,26 +1,26 @@
-package com.test.finalproject.controller;
-
-
-import javax.servlet.ServletContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-
-@Controller
-public class HomeController {
-	@Autowired ServletContext seco;
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-
-		seco.setAttribute("cp", seco.getContextPath());
-
-		return "home.tiles";
-
-	}
-	
-}
+//package com.test.finalproject.controller;
+//
+//
+//import javax.servlet.ServletContext;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//
+//@Controller
+//public class HomeController {
+//	@Autowired ServletContext seco;
+//
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Model model) {
+//
+//		seco.setAttribute("cp", seco.getContextPath());
+//
+//		return "home.tiles";
+//
+//	}
+//	
+//}
--- finalproject0124/src/main/java/com/test/finalproject/controller/HomeTestController.java
+++ finalproject0124/src/main/java/com/test/finalproject/controller/HomeTestController.java
@@ -1,5 +1,8 @@
 package com.test.finalproject.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeTestController {
+	@Autowired ServletContext seco;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String adminhome(Model model) {
+		seco.setAttribute("cp", seco.getContextPath());
 
 		return "hometest.tiles";
 
