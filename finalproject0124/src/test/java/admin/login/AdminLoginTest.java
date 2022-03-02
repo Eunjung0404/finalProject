f68c2452706ba70e985f8a80e5ceaa8a986c6641
u1b85 <lkw7333@gmail.com>
2022-03-02 18:49:19 +0900

    TEST - 트랜젝션 테스트 - IJ

--- finalproject0124/src/test/java/admin/login/AdminLoginTest.java
+++ finalproject0124/src/test/java/admin/login/AdminLoginTest.java
@@ -1,5 +1,7 @@
 package admin.login;
 
+import com.test.finalproject.form.AdminInfoForm;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.test.finalproject.form.AdminLoginForm;
 import com.test.finalproject.service.AdminService;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -21,10 +24,21 @@ public class AdminLoginTest {
 
 	@Autowired
 	private AdminService service;
-	
+
+	@Before
+	public void setAdmin() {
+		AdminInfoForm info = new AdminInfoForm();
+		info.setAid("test999");
+		info.setApwd("qwer1234!@");
+		info.setAlevel(1);
+		info.setTheatername("ALL");
+		service.insert(info);
+	}
+
 	@Test
+	@Transactional
 	public void 로그인성공() {
-		String inputId = "admin001";
+		String inputId = "test999";
 
 		AdminLoginForm form = new AdminLoginForm();
 		form.setAid(inputId);
@@ -38,6 +52,7 @@ public class AdminLoginTest {
 	}
 
 	@Test
+	@Transactional
 	public void 로그인실패() {
 		String inputId = "user001";
 
