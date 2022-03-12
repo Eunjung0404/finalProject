48bd7f86cba2af197202682793424dd4310dd7b2
u1b85 <lkw7333@gmail.com>
2022-03-12 21:24:38 +0900

    TEST - 계정 관리 API 테스트

--- finalproject0124/src/test/java/admin/info/InfoAPITest.java
+++ finalproject0124/src/test/java/admin/info/InfoAPITest.java
@@ -1,11 +1,14 @@
 package admin.info;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test.finalproject.form.AdminInfoForm;
 import com.test.finalproject.vo.AdminVo;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,7 +16,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -21,20 +28,22 @@ import org.springframework.web.context.WebApplicationContext;
         "file:src/main/webapp/WEB-INF/spring/security-config.xml",
         "file:src/main/webapp/WEB-INF/spring/tile-define.xml",
         "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@Transactional
 public class InfoAPITest {
 
     @Autowired
     private WebApplicationContext ctx;
 
     private MockMvc mockMvc;
-    private MockHttpSession session;
+    private static final MockHttpSession session = new MockHttpSession();;
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final AdminInfoForm form = setAdminInfoForm();
 
     @BeforeClass
-    public void setAdminSession() {
+    public static void setAdminSession() {
         AdminVo admin = new AdminVo();
         admin.setAlevel(1);
 
-        session = new MockHttpSession();
         session.setAttribute("admin", admin);
     }
 
@@ -45,26 +54,45 @@ public class InfoAPITest {
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    public void getList() {
-
+    public void getList() throws Exception {
+        mockMvc.perform(get("/admin/info/list").session(session))
+                .andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    public void postAdmin() {
-
+    public void postAdmin() throws Exception {
+        mockMvc.perform(post("/admin/info")
+                .session(session)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(form))
+            ).andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    public void putAdmin() {
-
+    public void putAdmin() throws Exception {
+        mockMvc.perform(put("/admin/info")
+                .session(session)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(form))
+        ).andExpect(status().isOk());
     }
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    public void deleteAdmin() {
+    public void deleteAdmin() throws Exception {
+        mockMvc.perform(delete("/admin/info/test999").session(session))
+                .andExpect(status().isOk());
+    }
 
+    private static AdminInfoForm setAdminInfoForm() {
+        AdminInfoForm admin = new AdminInfoForm();
+        admin.setAid("test999");
+        admin.setApwd("qwer1234!@");
+        admin.setAlevel(1);
+        admin.setTheatername("ALL");
+        return admin;
     }
 
 }
